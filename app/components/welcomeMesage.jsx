5dc296e067b80e23be5477ced18b523a015a441a
Einstein <forensicslabnet@gmail.com>
2020-02-11 05:57:09 +0300

    Add readme with badges

--- app/components/welcomeMesage.jsx
+++ app/components/welcomeMesage.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const WelcomeMessage = () => (
   <div className="welcomeMessage">
     <h1>Read as you Go.</h1>
-    Enrich your life, go anywhere, learn anything, read every day.
+    Enrich your life, go anywhere, read every day.
     <br />
-    Rent a book here and read as you go
+    Rent a book here at 1$ per book, per day
   </div>
 );
 
